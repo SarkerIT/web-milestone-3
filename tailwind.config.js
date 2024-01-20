9b8d39bdb588b1e4eab47a4f9dbc08b5d2ec4c39
SarkerIT <sarker.data@gmail.com>
2024-01-20 13:51:46 -0600

    basic setup done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+// for getting the tailwind intelliSense
+// you need this file tailwind.config.js
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
